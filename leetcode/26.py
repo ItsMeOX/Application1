d1c016565dd9f9f036a168310ac3c6972ebad6cd
ItsMeOX <85620202+ItsMeOX@users.noreply.github.com>
2023-04-25 18:16:57 +0800

    Delete leetcode directory

--- leetcode/26.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        left = 0
-        for x in nums:
-            if nums[left] != x:
-                left += 1
-                nums[left] = x
-        
-        return left + 1
